2d3ca54b9a2f296e16e75bbbbcf0f4ce608b89a6
josepagan <josepaganrnj@outlook.com>
2026-05-22 13:46:45 -0400

    Implemented sandwich topping pricing using category based enum logic.

--- src/main/java/Sandwich.java
+++ src/main/java/Sandwich.java
@@ -21,6 +21,24 @@ public class Sandwich implements Product {
             case FOUR -> total = 5.50;
             case EIGHT -> total = 7.00;
             case TWELVE -> total = 8.50;
+        }
+        for (Topping topping : toppings) {
+            if (topping.isPremium()) {
+                if (topping.getCategory() == ToppingCategory.MEAT) {
+                    switch (size) {
+                        case FOUR -> total += 1;
+                        case EIGHT -> total += 2;
+                        case TWELVE -> total += 3;
+                    }
+                }
+                if (topping.getCategory() == ToppingCategory.CHEESE) {
+                    switch (size) {
+                        case FOUR -> total += .75;
+                        case EIGHT -> total += 1.50;
+                        case TWELVE -> total += 2.25;
+                    }
+                }
+            }
 
         }
         return total;
